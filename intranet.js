eb0d9d192a05f88dd4feb4ad770d030fc9b3e8c1
manubop <manubop@gmail.com>
2019-03-07 01:22:46 +0100

    first commit

---
+++ intranet.js
@@ -0,0 +1,327 @@
+const https = require('https');
+const url = require('url');
+const querystring = require('querystring');
+const xml2js = require('xml2js'); 
+const winston = require('winston');
+const AsyncLock = require('async-lock');
+
+const logger = winston.createLogger({
+  level: process.env.LOGGER_LEVEL || 'info',
+  format: winston.format.json(),
+  defaultMeta: {service: 'user-service'},
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+var getCookieStr = function (cookies) {
+
+	return cookies ? cookies.filter(cookie => !cookie.includes('=deleted')).map(cookie => cookie.split(';')[0]).join('; ') : '';
+};
+
+var httpsPost = function (hostname, path, cookies, postData, handler) {
+
+	logger.log('verbose', 'post: ' + hostname + path);
+	logger.log('verbose', 'cookies: ' + cookies);
+
+	if (typeof postData !== 'string') {
+		postData = querystring.stringify(postData);
+	}
+
+	logger.log('verbose', 'postData: ' + postData);
+
+	let options = {
+		hostname: hostname,
+		path: path,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+       		'Content-Length': postData.length,
+       		'Cookie': cookies
+       	}
+	};
+
+	let req = https.request(options, (resp) => {
+
+		logger.log('verbose', 'statusCode: ' + resp.statusCode);
+		logger.log('verbose', JSON.stringify(resp.headers));
+
+		handler(resp);
+	});
+
+	req.on('error', (e) => {
+	  	handler(e);
+	});
+
+	req.write(postData);
+	req.end();
+}
+
+var httpsGet = function (hostname, path, cookies, handler) {
+
+	logger.log('verbose', 'get: ' + hostname + path);
+	logger.log('verbose', 'cookies: ' + cookies);
+
+	let options = {
+		hostname: hostname,
+		path: path,
+		headers: { 
+			'Cookie': cookies 
+		}
+	};
+
+	https.get(options, (resp) => {
+
+		logger.log('verbose', 'statusCode: ' + resp.statusCode);
+		logger.log('verbose', JSON.stringify(resp.headers));
+
+		let data = '';
+
+		resp.on('data', (chunk) => {
+			data += chunk;
+		});
+
+		resp.on('end', () => {
+			if (handler) {
+				handler(resp, data)
+			}
+		});
+	});
+}
+
+var getRedirection = function (hostname, location) {
+
+	if (location) {
+
+		let parsed = url.parse(location);
+
+		if (parsed.hostname) {
+
+			return {
+				hostname: parsed.hostname,
+				path: parsed.path
+			}
+		}
+
+		return {
+			hostname: hostname,
+			path: location
+		}
+	}
+
+	return null;
+}
+
+var IntranetSession = function (addr, username, password) {
+
+	var _cookiestr = '';
+	var _lock = new AsyncLock();
+
+	var handleLastRedirection = function (hostname, path, postData, done) {
+
+		httpsPost(hostname, path, '', postData, function (resp) {
+
+			if (resp.statusCode === 302) {
+
+				let redirect = getRedirection(hostname, resp.headers.location);
+
+				if (redirect) {
+
+					_cookiestr = getCookieStr(resp.headers["set-cookie"]);
+
+					httpsGet(redirect.hostname, redirect.path, _cookiestr, function (resp, data) {
+						done(false, { path: redirect.path, statusCode: resp.statusCode, body: data });
+					});
+
+				} else {
+
+					done(true);
+				}
+
+			} else {
+
+				done(true);
+			}
+		});
+	}
+
+	var handleSSO = function (hostname, path, cookiestr, done) {
+
+		httpsGet(hostname, path, cookiestr, function (resp, data) {
+
+			xml2js.parseString(data, function (err, result) {
+
+				if (result) {
+
+					let form = result.html.apm_do_not_touch[0].body[0].form[0];
+			    	let parsed = url.parse(form['$'].action);
+			    	let postData = {
+						[form.input[0]['$'].name]: form.input[0]['$'].value,
+						[form.input[1]['$'].name]: form.input[1]['$'].value
+					};
+
+					handleLastRedirection(parsed.hostname, parsed.path, postData, done);
+
+			    } else {
+
+					done(true);
+				}
+			});
+		});
+	};
+
+	var handleLogin = function (hostname, path, cookiestr, done) {
+
+		let postData = {
+			username: username,
+			password: password,
+			vhost: 'standard'
+		};
+
+		httpsPost(hostname, path, cookiestr, postData, function (resp) {			
+
+			if (resp.statusCode === 200) {
+
+				let cookiestr = getCookieStr(resp.headers["set-cookie"]);
+
+				httpsPost(hostname, path, cookiestr, postData, function (resp) {
+
+					if (resp.statusCode === 302) {
+
+						let redirect = getRedirection(hostname, resp.headers.location);
+
+						if (redirect) {
+
+							logger.log('verbose', 'redirecting to: ' + redirect.hostname + redirect.path);
+
+							let cookiestr = getCookieStr(resp.headers["set-cookie"]);
+
+							handleSSO(redirect.hostname, redirect.path, cookiestr, done);
+
+						} else {
+
+							done(true);
+						}
+
+					} else {
+
+						done(true);
+					}
+				});
+
+			} else {
+
+				done(true);
+			}			
+		});
+	};
+
+	var handleLogout = function (hostname, path, cookiestr, done) {
+
+		httpsGet(hostname, path, cookiestr, function (resp, data) {
+			
+			xml2js.parseString(data, function (err, result) {
+
+				if (result) {
+
+					let form = result.html.apm_do_not_touch[0].body[0].form[0];
+			    	let postData = {
+						[form.input[0]['$'].name]: form.input[0]['$'].value
+					};
+
+					httpsPost(hostname, path, '', postData, function(resp) {
+
+						done(false, resp);
+					});
+
+			    } else {
+
+					done(true);
+				}
+			});
+		});
+	}
+
+	var handleGetResponse = function (hostname, headers, done) {
+
+		let redirect = getRedirection(hostname, headers.location);
+
+		if (redirect) {
+
+			logger.log('verbose', 'redirecting to: ' + redirect.hostname + redirect.path);
+
+			let cookiestr = getCookieStr(headers["set-cookie"]);
+
+			if (redirect.path === '/my.policy') {
+
+				handleLogin(redirect.hostname, redirect.path, cookiestr, done);
+
+			} else if (redirect.path.indexOf('/saml/idp/profile/redirect/sls?SAMLRequest=') === 0) {
+
+				handleLogout(redirect.hostname, redirect.path, cookiestr, done);
+
+			} else {
+
+				httpsGet(redirect.hostname, redirect.path, cookiestr, function (resp, data) {
+					handleGetResponse(redirect.hostname, resp.headers, done);
+				});
+			}
+
+		} else {
+
+			done(true);
+		}
+	}
+
+	this.get = function (path) {
+
+		return new Promise(function (resolve, reject) {
+
+			_lock.acquire('cookie', function (done) {
+
+				httpsGet(addr, path, _cookiestr, function (resp, data) {
+
+					if (resp.statusCode === 302) {
+
+						handleGetResponse(addr, resp.headers, done);
+
+					} else {
+
+						done(false, { path: path, statusCode: resp.statusCode, body: data });
+					}
+				});
+				
+			}, function (err, data) {
+
+				if (!err) {
+					resolve(data);
+				} else {
+					reject();
+				}
+
+			}, {});
+		});
+	}
+
+	this.logout = function () {
+
+		return this.get('/Shibboleth.sso/Logout');
+	}
+}
+
+exports.IntranetSession = IntranetSession;
